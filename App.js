d8b43713908f7d374e43a8789552ee40e30be3f2
seema77 <skurade70@gmail.com>
2023-02-08 02:01:13 +0530

    app.js modified

--- App.js
+++ App.js
@@ -31,7 +31,7 @@ export default class App extends React.Component {
             style: { color: '#fff', fontSize: 20 },
           }}
         />
-
+<Text>hii new text is inserted</Text>
         <Image
           style={styles.imageIcon}
           source={{
